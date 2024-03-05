880f5ed7792b12d62a60eddd7d792447655432f6
mertaydin9999 <mertaydin9999@hotmail.com>
2024-03-05 08:57:53 +0300

    sayac il commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return <footer>© Copyright 2024 MAKEL - (4.64) - www.makel.com.tr</footer>;
+};
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>Anasayfa</li>
+        <li>Kullanici</li>
+        <li>Tesisat Semasi</li>
+        <li>Okumalar</li>
+        <li>Raporlar</li>
+        <li>Modem Islemleri</li>
+        <li>Uyari Mesajlari</li>
+        <li>Hesabim</li>
+        <li>Cikis</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
